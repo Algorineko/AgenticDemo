e7a8a0d88ac0966946dcc5f3fe85b63116049179
Algorineko <algorineko@gmail.com>
2026-02-09 23:34:22 +0800

    [chore] 测试PDF翻译

--- AgenticArxiv/config.py
+++ AgenticArxiv/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 import os
 
 try:
-    from dotenv import load_dotenv
+    from dotenv import load_dotenv  # pyright: ignore[reportMissingImports]
     load_dotenv()
 except Exception:
     # 也可以直接在终端 export 环境变量
@@ -18,7 +18,9 @@ class LLMModels:
 
 @dataclass(frozen=True)
 class Settings:
-    antigravity_base_url: str = os.getenv("LLM_BASE_URL", "https://antigravity.byssted.cn")
+    antigravity_base_url: str = os.getenv(
+        "LLM_BASE_URL", "https://antigravity.byssted.cn"
+    )
     antigravity_api_key: str = os.getenv("LLM_API_KEY", "no-token-here")
     models: LLMModels = LLMModels()
 
--- AgenticArxiv/readme.md
+++ AgenticArxiv/readme.md
@@ -9,6 +9,15 @@ LLM_API_KEY=<token>
 
 ### PDFMathTranslate
 ```sh
+# 下载 ArXiv 论文PDF
 export PDF_RAW_PATH=/home/dev/AgenticDemo/AgenticArxiv/output/pdf_raw 
 wget -P $PDF_RAW_PATH https://arxiv.org/pdf/2601.22156v1.pdf
 ```
+```sh
+# 进入GUI
+pdf2zg -i
+# 默认是英译中,-t为使用CPU核数
+pdf2zh ./pdf-raw/3690624.3709231.pdf -s bing -o ./pdf-translated/ [-p 1] [--debug] [-t 4]
+# case
+pdf2zh /home/dev/AgenticDemo/AgenticArxiv/output/pdf_raw/2601.22156v1.pdf -s bing -o /home/dev/AgenticDemo/AgenticArxiv/output/pdf_translated -t 3
+``` 
